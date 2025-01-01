7a4c5ab403ce7f23af7858f33d58cd143c36c372
Omijeh-David <om2918440@gmail.com>
2025-01-01 23:13:24 +0100

    Shinra Tensei

--- src/App.js
+++ src/App.js
@@ -1,26 +1,43 @@
 import React from "react";
 import './App.css';
-import myImage from './images/my-image.jpeg';
-
+import myImage from './images/my-image.png';
+import blenderImage from './images/blender.png';
 
 function App() {
   return (
     <div>
       <header>
-        <div class="nav-container">
+        <div className="nav-container">
           <h1>Omijeh David Odianonsen</h1>
         </div>
         <nav>
-          <a href="#about">About</a>
-          <a href="#projects">Projects</a>
-          <a href="#contact">Contact</a>
+          <a href="#about">
+            <i className="bi bi-github"></i>
+          </a>
+          <a href="#projects">
+            <i className="bi bi-twitter"></i>
+          </a>
+          <a href="#contact">
+            <i className="bi bi-instagram"></i>
+          </a>
         </nav>
       </header>
 
       <section id="hero">
+
+        <div>
+          <img src={blenderImage} alt="Instagram" className="floating-icon" />
+          <img src={blenderImage} alt="Instagram" className="floating-icon" />
+          <img src={blenderImage} alt="Instagram" className="floating-icon" />
+          <img src={blenderImage} alt="Instagram" className="floating-icon" /><img src={blenderImage} alt="Instagram" className="floating-icon" /><img src={blenderImage} alt="Instagram" className="floating-icon" /><img src={blenderImage} alt="Instagram" className="floating-icon" />
+        </div>
+
+
         <img src={myImage} alt="Hero" className="hero-image" />
-        <h2>Welcome to My Portfolio</h2>
-        <p>I am a web developer passionate about creating amazing experiences.</p>
+        <h2 style={{ color: 'white', fontSize: '3rem' }}>
+          I am <span>OMIJEH DAVID ODIANONSEN</span>
+        </h2>
+        {/* <p>I am a web developer passionate about creating amazing experiences.</p> */}
       </section>
 
       <section id="about">
